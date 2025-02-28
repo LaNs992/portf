be42d2a406ef3f292c140cacde7d6c5481278b45
lans992 <lans_play@mail.ru>
2025-02-28 09:55:40 +0300

    адаптив

--- src/componets/nav/navbar.jsx
+++ src/componets/nav/navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           <ul className="nav-list">
             <li className="nav-list__item">
               <NavLink
-                to="/proft"
+                to="/proft/"
                 className={(isActive) => {
                   return isActive ? activlink : normalink;
                 }}
