4130fc7aa76af4d27a6dec1b05888b8741027b45
lans992 <lans_play@mail.ru>
2025-02-28 10:25:59 +0300

    испарвлены

--- src/Helpers/projectlist.js
+++ src/Helpers/projectlist.js
@@ -1,4 +1,4 @@
-import pj1 from "./../img/projects/pj1.jpg";
+import pj1 from "./../img/projects/pjb1.png";
 import pj2 from "./../img/prod/topken.png";
 import pj3 from "./../img/prod/learnai.png";
 import pj5 from "./../img/prod/image.png";
@@ -8,13 +8,6 @@ import pjb3 from "./../img/prod/learnai.png";
 import pjb5 from "./../img/prod/image.png";
 
 const projects = [
-  {
-    title: "Сайт по поиску работы",
-    skills: "Php,Html,css,Js",
-    img: pj1,
-    imgbig: pjb1,
-    gitHubLink: "https://github.com/LaNs992/ProFinder-site",
-  },
   {
     title: "TokenSait",
     skills: "Html,css,Js",
@@ -29,7 +22,13 @@ const projects = [
     imgbig: pjb3,
     gitHubLink: "https://lans992.github.io/LearnGenA/",
   },
-
+  {
+    title: "Сайт по поиску работы",
+    skills: "Php,Html,css,Js",
+    img: pj1,
+    imgbig: pjb1,
+    gitHubLink: "https://github.com/LaNs992/ProFinder-site",
+  },
   {
     title: "Superdao",
     skills: "Html,css,",
